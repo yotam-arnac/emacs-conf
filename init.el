847356cb7eb6683f51945c5552d633ce5d9ac46f
Yoav Orot <orot.yoav@gmail.com>
2021-05-17 01:41:33 +0300

    Add key bindings for consult functions
    Key bindings are taken from consult's official readme.

--- init.el
+++ init.el
@@ -128,6 +128,52 @@ DIR must include a .project file to be considered a project."
 
 ;; Init consult for enhanced search commands
 (use-package consult
+  :demand t
+  :general
+  ;; C-c bindings (mode-specific-map)
+  ("C-c h" #'consult-history)
+  ("C-c m" #'consult-mode-command)
+  ("C-c b" #'consult-bookmark)
+  ("C-c k" #'consult-kmacro)
+  ;; C-x bindings (ctl-x-map)
+  ("C-x M-:" #'consult-complex-command)     ;; orig. repeat-complex-command
+  ("C-x b" #'consult-buffer)                ;; orig. switch-to-buffer
+  ("C-x 4 b" #'consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+  ("C-x 5 b" #'consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+  ;; Custom M-# bindings for fast register access
+  ("M-#" #'consult-register-load)
+  ("M-'" #'consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+  ("C-M-#" #'consult-register)
+  ;; Other custom bindings
+  ("M-y" #'consult-yank-pop)                ;; orig. yank-pop
+  ("<help> a" #'consult-apropos)            ;; orig. apropos-command
+  ;; M-g bindings (goto-map)
+  ("M-g e" #'consult-compile-error)
+  ("M-g f" #'consult-flymake)               ;; Alternative: consult-flycheck
+  ("M-g g" #'consult-goto-line)             ;; orig. goto-line
+  ("M-g M-g" #'consult-goto-line)           ;; orig. goto-line
+  ("M-g o" #'consult-outline)
+  ("M-g m" #'consult-mark)
+  ("M-g k" #'consult-global-mark)
+  ("M-g i" #'consult-imenu)
+  ("M-g I" #'consult-project-imenu)
+  ;; M-s bindings (search-map)
+  ("M-s f" #'consult-find)
+  ("M-s L" #'consult-locate)
+  ("M-s g" #'consult-grep)
+  ("M-s G" #'consult-git-grep)
+  ("M-s r" #'consult-ripgrep)
+  ("M-s l" #'consult-line)
+  ("M-s m" #'consult-multi-occur)
+  ("M-s k" #'consult-keep-lines)
+  ("M-s u" #'consult-focus-lines)
+  ;; Isearch integration
+  ("M-s e" #'consult-isearch)
+  (:keymaps 'org-mode-map
+   "M-e" #'consult-isearch)                 ;; orig. isearch-edit-string
+  ("M-s e" #'consult-isearch)               ;; orig. isearch-edit-string
+  ("M-s l" #'consult-line)                  ;; required by consult-line to detect isearch
+
   :config
   ;; Configure project detection using project.el
   (setq consult-project-root-function
