83ad2cb2c1827fdba12d169f5d4b52db19cf26e5
Yoav Orot <orot.yoav@gmail.com>
2021-06-07 01:43:25 +0300

    Add terraform support in lsp mode

--- init.el
+++ init.el
@@ -403,6 +403,7 @@ DIR must include a .project file to be considered a project."
                                   typescript-mode
                                   json-mode
                                   dockerfile-mode
+                                  terraform-mode
                                   sh-mode))
 
   (defun mo-maybe-enable-lsp (lsp-config)
