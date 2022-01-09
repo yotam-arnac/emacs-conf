4ec63cfd440b687ca72d6b735ef3a63bbc07ec94
Yoav Orot <orot.yoav@gmail.com>
2022-01-09 15:53:14 +0200

    Add lsp-haskell and register for support in lsp mode

--- init.el
+++ init.el
@@ -715,6 +715,8 @@ DIR must include a .project file to be considered a project."
                                   c++-mode
                                   objc-mode
                                   swift-mode
+                                  haskell-mode
+                                  haskell-literate-mode
                                   go-mode
                                   csharp-mode
                                   java-mode
@@ -870,6 +872,9 @@ run the attached function (if exists) and enable lsp"
 
 ;; Init haskell-mode for Haskell support
 (use-package haskell-mode)
+
+;; Init lsp-haskell for Haskell language server
+(use-package lsp-haskell)
 
 ;; Init go-mode for Go support
 (use-package go-mode)
