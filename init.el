58fa3d0f4074fae8d8992560d9ea436d1f9bca38
Yoav Orot <orot.yoav@gmail.com>
2022-02-05 18:07:04 -0600

    helpful: Add helpful commands key bindings to the quick menu

--- init.el
+++ init.el
@@ -1046,6 +1046,9 @@ run the attached function (if exists) and enable lsp"
   ("C-h C" #'helpful-command)
   (:keymaps 'mo-quick-menu-map
    :prefix "h"
+   "f" #'helpful-callable
+   "v" #'helpful-variable
+   "k" #'helpful-key
    "h" #'helpful-at-point))
 
 ;; Init google-this for quick Google searches from Emacs
