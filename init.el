76b1d8bd125e392d6b69150bd970e9ffe792c274
Yoav Orot <orot.yoav@gmail.com>
2022-03-24 19:13:30 -0500

    theme: Remove custom buffer margins color

--- init.el
+++ init.el
@@ -1184,8 +1184,6 @@ run the attached function (if exists) and enable lsp"
   (load-theme 'doom-one t)
   ;; Distinguish between var reads and writes by underlining lsp write highlights
   (set-face-attribute 'lsp-face-highlight-write nil :underline t)
-  ;; Set buffer margins background color
-  (set-face-attribute 'fringe nil :background (face-attribute 'mode-line :background))
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config)
   ;; Load icons here, so their background will be aligned with the theme
