0dee1bbd8d89e05ef717d323b02acf1f9e3e306e
Yoav Orot <orot.yoav@gmail.com>
2022-04-07 19:50:34 -0400

    avy: Change key bindings to C-' / C-"
    M-p is already used by some packages.

--- init.el
+++ init.el
@@ -239,8 +239,8 @@ Ask for action even on single candidate jumps."
     (let ((avy-single-candidate-jump nil))
       (avy-goto-char-timer)))
   :general
-  ("M-p" #'avy-goto-char-timer
-   "M-P" #'mo-avy-goto-char-timer-action)
+  ("C-'" #'avy-goto-char-timer
+   "C-\"" #'mo-avy-goto-char-timer-action)
   :config
   ;; Better highlight the leading characters
   (set-face-attribute 'avy-lead-face nil :background "gold1")
